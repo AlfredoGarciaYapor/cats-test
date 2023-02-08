57037b6a633cb2c910b69f26c7603e6f7084285a
Alfredo Garcia <60104086+AlfredoGarciaYapor@users.noreply.github.com>
2023-02-08 14:28:01 -0700

    last details

--- cats-facts-test/src/components/Body.jsx
+++ cats-facts-test/src/components/Body.jsx
@@ -25,7 +25,7 @@ const Body = () => {
     <>
       <div className="App bg-[#aea8a8]  h-screen flex flex-col justify-center items-center">
         <div className=" m-3 flex flex-col h-full w-full items-center ">
-          <form className="my-8 flex justify-center" onSubmit={handleContinue}>
+          <form className="my-8 flex items-center justify-center" onSubmit={handleContinue}>
             <Input
               type="text"
               value={name}
@@ -37,7 +37,7 @@ const Body = () => {
             <button
               type="submit"
               onClick={handleContinue}
-              className="text-white mx-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white mx-3 bg-[#02DAFF] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Continue
             </button>
